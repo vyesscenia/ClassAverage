1036a361295f25c2a20002a9b77abe06587c44a9
Your Name <vyesscenia@mail.fresnostate.edu>
2021-05-13 21:10:50 -0700

    finished structured english

---
+++ class_average.py
@@ -0,0 +1,12 @@
+"""
+input:the test scores
+initiate a iterator (counter) and accumulator (sum) and set it to zero
+loop through the list test scores
+add all the test scores
+increment the counter by one
+once the loop ends, there are no more scored to add
+divide the accumilator (sum) by counter
+display the output to the user
+output:print the average of the class scores
+
+"""
